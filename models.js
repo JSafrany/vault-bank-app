be6b707d0e3a62688573fa0fb31874979ceb5ce2
Yuna Lee <yunalee@fb.com>
2021-01-28 11:08:35 +0000

    models edited

--- models.js
+++ models.js
@@ -1,13 +1,22 @@
 const { Sequelize, Model, DataTypes } = require('sequelize')
 const path = require('path')
 
+const connectionSettings = {
+    test: {dialect: 'sqlite', storage: 'sqlite::memory:'},
+    dev: {dialect: 'sqlite', storage: path.join(__dirname, 'data.db')},
+    production: {dialect: 'postgres', protocal: 'postgres'}
+}
+const sequelize = process.env.NODE_ENV === 'production'
+    ? new Sequelize(process.env.DATABASE_URL, connectionSettings[process.env.NODE_ENV])
+    : new Sequelize(connectionSettings[process.env.NODE_ENV])
+
 class User extends Model {}
 class TransactionHistory extends Model {}
 class Friend extends Model{}
 
 User.init({
-    firstname: DataTypes.STRING,
-    lastname: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
     password: DataTypes.STRING,
     email: DataTypes.STRING,
     balance: DataTypes.FLOAT,
@@ -18,8 +27,7 @@ TransactionHistory.init ({
     from: DataTypes.STRING,
     to: DataTypes.STRING,
     amount: DataTypes.STRING,
-
-})
+}, {sequelize: sequelize})
 
 
 User.hasMany(TransactionHistory, {as:"history"})
