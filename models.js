14959edbe3a8d87e46594af36125b0ae0fdbdd6e
jsafrany <j.c.safrany@gmail.com>
2021-01-28 11:33:31 +0000

    fixed models

--- models.js
+++ models.js
@@ -6,9 +6,10 @@ const connectionSettings = {
     dev: {dialect: 'sqlite', storage: path.join(__dirname, 'data.db')},
     production: {dialect: 'postgres', protocal: 'postgres'}
 }
+
 const sequelize = process.env.NODE_ENV === 'production'
-    ? new Sequelize(process.env.DATABASE_URL, connectionSettings[process.env.NODE_ENV])
-    : new Sequelize(connectionSettings[process.env.NODE_ENV])
+    ? new Sequelize(process.env.DATABASE_URL, {dialect: 'sqlite', storage: path.join(__dirname, 'data.db')})
+    : new Sequelize({dialect: 'sqlite', storage: path.join(__dirname, 'data.db')})
 
 class User extends Model {}
 class TransactionHistory extends Model {}
@@ -19,7 +20,7 @@ User.init({
     password: DataTypes.STRING,
     email: DataTypes.STRING,
     balance: DataTypes.REAL,
-    friends: DataTypes.ARRAY,
+    friends: DataTypes.ARRAY(DataTypes.STRING),
 }, {sequelize: sequelize})
 
 TransactionHistory.init ({
